f0949be6d2d8c2787f9fe43436a622a0bea0fa1a
baf8008 <baf0618@gmail.com>
2021-08-31 16:13:53 +0900

    TODOアプリ

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::orderByRaw('`deadline` IS NULL ASC')->orderBy('deadline')->get();
+        return view('todos.index', ['todos' => $todos]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'newTodo' => 'required|max:100',
+            'newDeadline' => 'nullable|after:"now"'
+        ]);
+        Todo::create([
+            'todo' => $request->newTodo,
+            'deadline' => $request->newDeadline
+        ]);
+
+        return redirect()->route('todos.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $todo = Todo::find($id);
+
+        return view('todos.edit',['todo' => $todo]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'update' => 'required|max:100',
+            'update' => 'nullable|after:"now"'
+        ]);
+        $todo = Todo::find($id);
+        $todo->todo = $request->updateTodo;
+        $todo->deadline = $request->updateDeadline;
+        $todo->save();
+
+        return redirect()->route('todos.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $todo = Todo::find($id);
+        $todo->delete();
+        return redirect()->route('todos.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::resource('/todos', TodoController::class)->middleware('auth');
+
+require __DIR__.'/auth.php';
+
+
+
+
